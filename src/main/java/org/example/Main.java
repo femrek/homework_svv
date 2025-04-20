86a719d1421881d3cb2f30afe5d0c8d25e76fd4e
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2025-04-20 13:28:19 +0300

    feat: printing stars randomly and periodically.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        final Random r = new Random();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                int n = r.nextInt(1, 15);
+                for (int i = 0; i < n; i++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        };
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(task, 0, 1000);
     }
 }
